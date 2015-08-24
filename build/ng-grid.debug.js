4520092eec98657ff10db875a01b8f27c19d0de2
akekaluck <akekaluck.wesnarat@thomsonreuters.com>
2015-08-24 16:00:58 +0700

    rebuild

--- build/ng-grid.debug.js
+++ build/ng-grid.debug.js
@@ -2,7 +2,7 @@
 * ng-grid JavaScript Library
 * Authors: https://github.com/angular-ui/ng-grid/blob/master/README.md 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 08/24/2015 15:51
+* Compiled At: 08/24/2015 16:00
 ***********************************************/
 (function(window, $) {
 'use strict';
